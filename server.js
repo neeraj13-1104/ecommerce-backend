7b5b00c767ce38ec69ec3e6938360f4ef4ef05a7
Neeraj Lalwanmi <neerajlalwwani620@gmail.com>
2026-02-03 17:29:12 +0530

    change origin url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000; // ✅ 5000 use karo
 // 🔥 CORS (MUST BE FIRST)
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://ecommerce-frontend-psi-mocha.vercel.app", "https://ecommerce-frontend-mzry.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),

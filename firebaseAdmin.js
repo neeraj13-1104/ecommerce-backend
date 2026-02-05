98b4d3073921d4d14c3750840c516e5ef8cb3b79
Neeraj Lalwanmi <neerajlalwwani620@gmail.com>
2026-02-05 16:03:11 +0530

    change

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,22 +1,13 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// __dirname fix for ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// read service account JSON
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, "serviceAccountKey.json"),
-    "utf8"
-  )
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 export default admin;
